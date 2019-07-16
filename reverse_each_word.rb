35b248c0eef9194700fd235e71c59318abe27bae
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-16 21:19:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,11 @@
+=begin'
 def reverse_each_word(sentence)
 new_sentence = sentence.reverse
 new_array = new_sentence.split
 last_sentence = new_array.reverse
 last_sentence.join(" ")
 end
+=end
 
 def reverse_each_word(sentence)
   sentence.each do |sent|
